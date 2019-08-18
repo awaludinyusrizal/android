22ea5422a80d3f70805220ef660a8466a93f90ef
AwaludinYusrizal <awaludin.yusrizal@gmail.com>
2019-08-18 12:44:45 +0700

    TestUI

---
+++ app/src/main/java/com/awal/latihan2/MainActivity.java
@@ -0,0 +1,28 @@
+package com.awal.latihan2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnLapor;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLapor = findViewById(R.id.btn_click);
+
+        btnLapor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "ini buton lapor", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+    }
+}
